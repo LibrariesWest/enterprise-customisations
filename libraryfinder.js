ce1c5a7bac55cc3b732bb3d5456fc00db39b767d
DaveBathnes <dave_roweuk@yahoo.co.uk>
2018-02-02 12:32:28 +0000

    working on library finder

--- libraryfinder.js
+++ libraryfinder.js
@@ -1,96 +1,108 @@
 ﻿$(function () {
 
-    var librarymap = L.map('map').setView([51.505, -0.09], 13);
+    $.getScript("leaflet.geometryutil.js", function (data, textStatus, jqxhr) {
 
-    var colours = {
-        'Bath and North East Somerset': { colour: [255, 99, 132] },
-        'Somerset': { colour: [54, 162, 235] },
-        'North Somerset': { colour: [255, 206, 86] },
-        'South Gloucestershire': { colour: [75, 192, 192] },
-        'Dorset': { colour: [153, 102, 255] },
-        'Poole': { colour: [255, 159, 64] },
-        'Bristol': { colour: [255, 204, 0] }
-    };
+        var librarymap = L.map('map').setView([51.505, -0.09], 13);
 
-    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}@2x.png', {
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution" > CARTO</a>',
-        maxZoom: 18,
-        id: 'light_all',
-    }).addTo(librarymap);
+        var colours = {
+            'Bath and North East Somerset': { colour: [255, 99, 132] },
+            'Somerset': { colour: [54, 162, 235] },
+            'North Somerset': { colour: [255, 206, 86] },
+            'South Gloucestershire': { colour: [75, 192, 192] },
+            'Dorset': { colour: [153, 102, 255] },
+            'Poole': { colour: [255, 159, 64] },
+            'Bristol': { colour: [255, 204, 0] }
+        };
 
-    var clickLibrary = function (library, e) {
-        $('#library').empty();
-        $('#library').append(
-            '<h4>' + library.properties.name + '</h4>' +
-            '<p class="small">A ' + library.properties.authority + ' at ' + library.properties.location + ' library</p>' +
-            '<p>Further library details here including opening hours, image, and address.</p>'
-        );
-    };
+        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/{id}/{z}/{x}/{y}@2x.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution" > CARTO</a>',
+            maxZoom: 18,
+            id: 'light_all',
+        }).addTo(librarymap);
 
-    var libraries = null, librariesdt = [];
+        var clickLibrary = function (library, e) {
+            $('#library').empty();
+            $('#library').append(
+                '<h4>' + library.properties.name + '</h4>' +
+                '<p class="small">A ' + library.properties.authority + ' owned at ' + library.properties.location + ' library</p>' +
+                '<p>Further library details here including opening hours, image, and address.</p>'
+            );
+        };
 
-    $.ajax({
-        dataType: "json",
-        url: "libraries.json",
-        success: function (data) {
+        var libraries = null, librariesdt = [];
+        var librariesarray = [];
 
-            $.each(data.features, function (i, f) {
-                librariesdt.push([f.properties.name, f.properties.authority, f.properties.location, f.properties.postcode, f.properties.email, f.properties.website, 0]);
-            });
+        $.ajax({
+            dataType: "json",
+            url: "libraries.json",
+            success: function (data) {
 
-            // Load the data
-            libraries = new L.geoJson(data, {
-                pointToLayer: function (feature, latlng) {
-                    var c = colours[feature.properties.authority.replace('NE', 'North East')].colour;
-                    return L.circleMarker(latlng, {
-                        radius: 5,
-                        fillColor: 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')',
-                        color: "#ccc",
-                        opacity: 1,
-                        weight: 1,
-                        fillOpacity: 1
-                    });
-                },
-                onEachFeature: function (feature, layer) {
-                    layer.on('click', clickLibrary.bind(this, feature));
-                }
-            });
-            libraries.addTo(librarymap);
+                $.each(data.features, function (i, f) {
+                    librariesdt.push([f.properties.name, f.properties.authority, f.properties.location, f.properties.postcode, f.properties.email, f.properties.website, 0]);
+                });
 
-            $('#tbl-libraries').DataTable({
-                data: librariesdt,
-                columns: [
-                    { title: 'Name' },
-                    { title: 'Authority' },
-                    { title: 'Address' },
-                    { title: 'Postcode' },
-                    {
-                        title: 'Email',
-                        render: function (data, type, row) {
-                            return '<a href="mailto:' + data + '" target="_blank">Email</a>';
-                        },
+                // Load the data
+                libraries = new L.geoJson(data, {
+                    pointToLayer: function (feature, latlng) {
+                        var c = colours[feature.properties.authority.replace('NE', 'North East')].colour;
+                        var marker = L.circleMarker(latlng, {
+                            radius: 5,
+                            fillColor: 'rgb(' + c[0] + ',' + c[1] + ',' + c[2] + ')',
+                            color: "#ccc",
+                            opacity: 1,
+                            weight: 1,
+                            fillOpacity: 1
+                        });
+                        librariesarray.push(marker);
+                        return marker;
                     },
-                    {
-                        title: 'Website',
-                        render: function (data, type, row) {
-                            return '<a href="' + data + '" target="_blank">Website</a>';
+                    onEachFeature: function (feature, layer) {
+                        layer.on('click', clickLibrary.bind(this, feature));
+                    }
+                });
+                libraries.addTo(librarymap);
+
+                $('#tbl-libraries').DataTable({
+                    data: librariesdt,
+                    columns: [
+                        { title: 'Name' },
+                        { title: 'Authority' },
+                        { title: 'Address' },
+                        { title: 'Postcode' },
+                        {
+                            title: 'Email',
+                            render: function (data, type, row) {
+                                return '<a href="mailto:' + data + '" target="_blank"><i class="fas fa-at"></i>&nbsp;Email</a>';
+                            },
                         },
-                    },
-                    { title: 'Distance' }
-                ]
-            });
-            librarymap.fitBounds(libraries.getBounds());
-        }
-    }).error(function (err) {
-        console.log(err);
+                        {
+                            title: 'Website',
+                            render: function (data, type, row) {
+                                return '<a href="' + data + '" target="_blank"><i class="fas fa-external-link-alt"></i>&nbsp;Website</a>';
+                            },
+                        },
+                        { title: 'Distance' }
+                    ]
+                });
+                librarymap.fitBounds(libraries.getBounds());
+            }
+        }).error(function (err) {
+            console.log(err);
         });
 
-    // Find nearest
-    $('#btn-search').on('click', function () {
-        // Get postcode lookup
-        $.getJSON('https://api.postcodes.io/postcodes/' + $('#txt-search').val(), function (data) {
-            // data.results
-            alert(data);
+        // Find nearest
+        $('#btn-search').on('click', function () {
+            var postcode = $('#txt-postcode').val();
+            if (postcode.length > 3) {
+                $.getJSON('https://api.postcodes.io/postcodes/' + postcode, function (data) {
+                    var longitude = data.result.longitude;
+                    var latitude = data.result.latitude;
+                    var m = L.GeometryUtil.closestLayer(librarymap, librariesarray, L.latLng(latitude, longitude), false).layer;
+                    librarymap.flyTo([m.feature.geometry.coordinates[1], m.feature.geometry.coordinates[0]], 15);
+                    m.openPopup();
+                });
+            }
         });
+
     });
 });
